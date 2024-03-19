a08c18ea7b83c019a215082faaacf3432a8599b7
Teslyak <124871929+Teslyak@users.noreply.github.com>
2024-03-19 10:18:34 +0200

    fix favoriteLiss overflow-y

--- src/Pages/Favorites/Favorite.styled.jsx
+++ src/Pages/Favorites/Favorite.styled.jsx
@@ -3,6 +3,5 @@ import styled from 'styled-components';
 export const Wrap = styled.div`
   position: relative;
   width: 1440px;
-  background: rgb(248, 248, 248);
-  padding: 96px 128px 96px 128px;
+ 
 `;
--- src/component/Favorites/FavoriteList.styled.jsx
+++ src/component/Favorites/FavoriteList.styled.jsx
@@ -1,8 +1,12 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const StyleList = styled.ul`
   width: 100%;
   display: flex;
   flex-direction: column;
   gap: 32px;
+  background: rgb(248, 248, 248);
+  padding: 96px 128px 96px 128px;
+  overflow-y: auto;
+  height: 100vh;
 `;
